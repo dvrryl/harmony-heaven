a7e3c67a2ba4b3c9bda6ccf32fe6a431e5fcd22e
AKMAL ASSEGAF <akmalassegaf5@gmail.com>
2023-10-23 09:13:37 +0700

    popup

---
+++ Harmony Heaven/Assets/Code/Scripts/ButtonPopUp.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPopUp : MonoBehaviour
+{
+    public Button buttonPrefab;
+    public Transform popUpLocation; // Objek Transform yang akan dijadikan titik pop-up
+    public float interval = 5.0f; // Interval waktu (dalam detik)
+    
+    private Button currentButton; // Tombol yang sedang muncul
+
+    void Start()
+    {
+        // Memulai proses popup otomatis
+        InvokeRepeating("TogglePopup", interval, interval);
+    }
+
+    // Fungsi ini akan dipanggil untuk menampilkan atau menyembunyikan tombol secara otomatis
+    private void TogglePopup()
+    {
+        if (currentButton != null)
+        {
+            // Jika ada tombol yang ditampilkan, sembunyikan
+            HideButton();
+        }
+        else
+        {
+            // Jika tidak ada tombol yang ditampilkan, tampilkan
+            ShowButton();
+        }
+    }
+
+    // Fungsi ini akan dipanggil untuk menampilkan tombol di posisi popUpLocation
+    public void ShowButton()
+    {
+        // Buat salinan tombol dari prefab
+        currentButton = Instantiate(buttonPrefab, popUpLocation);
+
+        // Atur posisi tombol sesuai dengan posisi popUpLocation
+        currentButton.transform.position = popUpLocation.position;
+    }
+
+    // Fungsi ini akan dipanggil untuk menyembunyikan tombol
+    public void HideButton()
+    {
+        if (currentButton != null)
+        {
+            Destroy(currentButton.gameObject);
+            currentButton = null;
+        }
+    }
+}
---
+++ Harmony Heaven/Assets/Code/Scripts/PopUp.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUp : MonoBehaviour
+{
+    public Button buttonPrefab;
+    public Transform popUpLocation; // Objek Transform yang akan dijadikan titik pop-up
+    public KeyCode Keyboard; // Tombol yang akan digunakan untuk manual popup
+
+    private Button currentButton; // Tombol yang sedang muncul
+
+    // Fungsi ini akan dipanggil untuk menampilkan tombol di posisi popUpLocation
+    public void ShowButton()
+    {
+        // Cek apakah ada tombol yang sedang ditampilkan
+        if (currentButton != null)
+        {
+            // Jika ada, sembunyikan tombol yang sedang ditampilkan
+            HideButton();
+        }
+
+        // Buat salinan tombol dari prefab
+        currentButton = Instantiate(buttonPrefab, popUpLocation);
+
+        // Atur posisi tombol sesuai dengan posisi popUpLocation
+        currentButton.transform.position = popUpLocation.position;
+    }
+
+    // Fungsi ini akan dipanggil untuk menyembunyikan tombol
+    public void HideButton()
+    {
+        if (currentButton != null)
+        {
+            Destroy(currentButton.gameObject);
+            currentButton = null;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(Keyboard))
+        {
+            // Toggle manual popup dan hide saat tombol ditekan
+            if (currentButton != null)
+            {
+                HideButton();
+            }
+            else
+            {
+                ShowButton();
+            }
+        }
+    }
+}
