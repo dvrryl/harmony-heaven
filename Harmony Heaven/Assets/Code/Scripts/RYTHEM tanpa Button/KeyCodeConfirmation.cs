320734476fd74d8aeac153607fff5267d1d99820
AKMAL ASSEGAF <akmalassegaf5@gmail.com>
2023-10-24 11:06:27 +0700

    a

--- Harmony Heaven/Assets/Code/Scripts/RYTHEM tanpa Button/KeyCodeConfirmation.cs
+++ Harmony Heaven/Assets/Code/Scripts/RYTHEM tanpa Button/KeyCodeConfirmation.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 
 public class KeyCodeConfirmation : MonoBehaviour
 {
-    public KeyboardDisplay keyboardDisplay; // Referensi ke script KeyboardDisplay
-    public KeyCodeKeyboard keyCodeKeyboard; // Referensi ke script KeyCodeKeyboard
+    public FixedkeyboardDisplay fixedkeyboardDisplay; // Referensi ke script KeyboardDisplay
+    public FixedKeyCodeKeyboard fixedkeyCodeKeyboard; // Referensi ke script KeyCodeKeyboard
 
     void Start()
     {
@@ -17,15 +17,15 @@ public class KeyCodeConfirmation : MonoBehaviour
     {
         yield return new WaitForSeconds(2.0f); // Tunggu selama 2 detik
 
-        if (keyboardDisplay != null && keyCodeKeyboard != null)
+        if (fixedkeyboardDisplay != null && fixedkeyCodeKeyboard != null)
         {
             Debug.Log("Mendapatkan informasi ImageComponent...");
-            int numberOfImageComponents = keyboardDisplay.imageComponents.Length;
+            int numberOfImageComponents = fixedkeyboardDisplay.imageComponents.Length;
             Debug.Log("Jumlah ImageComponent: " + numberOfImageComponents);
 
             for (int i = 0; i < numberOfImageComponents; i++)
             {
-                Image imageComponent = keyboardDisplay.imageComponents[i];
+                Image imageComponent = fixedkeyboardDisplay.imageComponents[i];
                 Sprite imageSprite = imageComponent.sprite;
 
                 Debug.Log("Mengecek ImageSprite pada ImageComponent " + i + "...");
@@ -65,9 +65,9 @@ public class KeyCodeConfirmation : MonoBehaviour
 
     private int FindSpriteIndex(string spriteName)
     {
-        for (int i = 0; i < keyboardDisplay.imageSprites.Length; i++)
+        for (int i = 0; i < fixedkeyboardDisplay.imageSprites.Length; i++)
         {
-            if (spriteName == keyboardDisplay.imageSprites[i].name)
+            if (spriteName == fixedkeyboardDisplay.imageSprites[i].name)
             {
                 return i;
             }
